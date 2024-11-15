aca0db1d15a2401d7d42d955aa9d9c838311465b
siyeon kim <91688002+sxxyxx127@users.noreply.github.com>
2024-11-15 21:15:59 +0900

    first push
    push

---
+++ CodingTest/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class Solution
+{
+	// "mm:ss" 형식의 문자열을 초 단위로 변환
+	private static int ToSecond(string timeStr) // "mm:ss"를 timeStr 매개변수
+	{
+		var parts = timeStr.Split(':'); //parts 문자열 배열에 분,초 저장
+		int minutes = int.Parse(parts[0]); //minutes 정수형에 분을 저장하기 위해 Parse 사용
+		int seconds = int.Parse(parts[1]);
+		return minutes * 60 + seconds; //초로 변환하여 반환
+	}
+
+	// 초 단위의 정수형 변수를 "mm:ss" 형식의 문자열로 변환
+	private static string ToMmSs(int totalSeconds)
+	{
+		int minutes = totalSeconds / 60;
+		int seconds = totalSeconds % 60;
+		return $"{minutes}:{seconds}"; //문자열 보간 사용
+	}
+
+	//비디오의 현재 재생 위치 업데이트 & "mm:ss" 반환
+	public string UpdateVideoPosition(string video_len, string pos, string op_start, string op_end, string[] commands)
+	//매개변수 : 비디오의 전체 길이, 현재 재생 위치, 오프닝 시작 구간, 오프닝 엔딩 구간, 입력 명령어 배열)
+	{
+		int video_len_sec = ToSecond(video_len);
+		int current_Pos = ToSecond(pos);
+		int op_Start_Sec = ToSecond(op_start);
+		int op_End_Sec = ToSecond(op_end);
+
+		foreach (string command in commands)
+		{
+			if (command == "prev")
+			{
+				current_Pos = Math.Max(0, current_Pos - 10);
+				//Math.Max : 두 값 중 더 큰 값을 반환
+			}
+			else if (command == "next")
+			{
+				current_Pos = Math.Min(video_len_sec, current_Pos + 10);
+			}
+
+			if (op_Start_Sec <= current_Pos && current_Pos <= op_End_Sec)
+			{
+				current_Pos = op_End_Sec;
+			}
+		}
+
+		return ToMmSs(current_Pos);
+	}
+
+	public class Start
+	{
+		public static void Main()
+		{
+			Solution solution = new Solution();
+			string video_len = "5:30";
+			string pos = "1:20";
+			string op_start = "0:10";
+			string op_end = "0:20";
+			string[] commands = { "prev", "next", "prev", "next" };
+
+			string result = solution.UpdateVideoPosition(video_len, pos, op_start, op_end, commands);
+			Console.WriteLine(result);
+
+		}
+	}
+
+}
---
+++ CodingeTest01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic; // namespace 가져오는 선언 -> 리스트, 딕셔너리, 스ㅡ택, 큐 등의 제네릭 컬렉션 클래스 제공
+
+public class Solution
+{
+    public static string[] solution1(string[] players, string[] callings)
+    {
+
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+        for (int i = 0; i < players.Length; i++)
+        {
+            dict[players[i]] = i;
+            //dict = {{"a",0},{"b",1},{"c",2},{"d",3}}
+        }
+        foreach (string e in callings)
+        {
+            //dict 에서의 순위 관련 변경
+            int index = dict[e];
+            dict[e]--;
+            dict[players[index - 1]]++;
+
+            //실질적인 players 배열에서의 순위 변경
+            string temp = players[index];
+            players[index] = players[index - 1];
+            players[index - 1] = temp;
+        }
+        return players;
+    }
+
+    public class Start
+    {
+        public static void Main()
+        {
+            string[] players = { "a", "b", "c", "d" };
+            string[] callings = { "b", "d", "d" };
+            string[] result = Solution.solution1(players, callings);
+            Console.WriteLine(string.Join(", ", result));
+            /*** 배열은 콘솔 입출력에 바로 적용하면 타입 정보가 출력된다. 
+            따라서, 배열을 바로 Console.WriteLine에 적용하려면 배열을 문자열로 변환한다.
+            -> Console.WriteLine(string.Join(",",result) 와 같은 방법 사용 ***/
+        }
+    }
+}
