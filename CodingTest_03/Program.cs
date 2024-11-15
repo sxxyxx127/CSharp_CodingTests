8b55bad0feb8284830e4b8603a0241da93c50ae6
siyeon kim <91688002+sxxyxx127@users.noreply.github.com>
2024-11-15 22:52:55 +0900

    test03
    test 03

---
+++ CodingTest_03/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+public class Solution
+{
+    public static string Cards(string[] cards1, string[] cards2, string[] goal)
+    {
+        int c1 = 0;
+        int c2 = 0;
+
+        for(int i = 0; i <goal.Length; i++)
+        {
+            if(c1<cards1.Length)
+            {
+                if(cards1[c1] == goal[i])
+                {
+                    c1++;
+                    continue;
+                }
+            }
+            if(c2<cards2.Length)
+            {
+                if (cards2[c2] == goal[i])
+                {
+                    c2++;
+                    continue;
+                }
+            }
+            return "No";
+
+        }
+        return "Yes";
+        
+    }
+
+    public class Start
+    {
+        public static void Main()
+        {
+            string[] cards1 = {"i","drink","water"};
+            string[] cards2 = {"want","to"};
+            string[] goal = {"i","want","to","drink","water"};
+            string result = Solution.Cards(cards1, cards2, goal);
+            Console.WriteLine(result);
+        }
+    }
+
+}
